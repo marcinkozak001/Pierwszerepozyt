76c003fa01adad332ef242ed25c075bae58504aa
Michalkowalski001 <34210655+Michalkowalski001@users.noreply.github.com>
2017-12-03 14:18:39 +0100

    Add files via upload

--- Plik1.c
+++ Plik1.c
@@ -1,5 +1,5 @@
-//CO TO JESST ZA progrtam ja siê pytam???
-// Data: 11.02.2012
+// Program testowy dla list jednokierunkowych
+// WOWOWOOWOOWWWo jest cos tu nowego ?
 // (C)2012 mgr Jerzy Wa³aszek
 //-------------------------------------------
 
